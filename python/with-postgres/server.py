919196075567d382ae8627f69ca55156b6469e11
Jake Cooper <jake.elijah.cooper@gmail.com>
2020-07-31 13:28:01 -0700

    Python example using our package (#7)
    * Uses the railway python package, installable via pip

---
+++ python/with-postgres/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from railway import postgres
+import os
+
+conn = postgres.conn
+cur = conn.cursor()
+try:
+    cur.execute("SELECT now()")
+except:
+    print("Oops database fail")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
