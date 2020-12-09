33a69351d99ef8be8519897c7cc37cdf1d77fe77
Jake Runzer <jakerunzer@gmail.com>
2020-12-09 15:08:02 -0700

    use port from env

--- elixir/with-cowboy/config/config.exs
+++ elixir/with-cowboy/config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :with_cowboy, WithCowboy.Endpoint, port: 4000
+config :with_cowboy, WithCowboy.Endpoint, port: String.to_integer(System.get_env("PORT") || "4000")
 config :with_cowboy, redirect_url: "http://localhost:4000/api"
 
 # import_config "#{Mix.env()}.exs"
