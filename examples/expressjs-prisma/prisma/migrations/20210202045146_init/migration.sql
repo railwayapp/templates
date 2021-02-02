1bb776c41ec3c640fa151c74e81ce47e620616a2
Jake Runzer <jakerunzer@gmail.com>
2021-02-01 22:18:33 -0700

    expressjs-prisma example

---
+++ examples/expressjs-prisma/prisma/migrations/20210202045146_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "text" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL,
+
+    PRIMARY KEY ("id")
+);
