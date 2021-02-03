6f580767942a4bf32ff09cbbf8e2a1c55825a1c0
Jake Runzer <jakerunzer@gmail.com>
2021-02-02 23:36:58 -0700

    update expressjs-postgres example to typescript

--- examples/expressjs-prisma/src/index.ts
+++ examples/expressjs-prisma/src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bodyParser from "body-parser";
+import express from "express";
 
 const prisma = new PrismaClient();
 
