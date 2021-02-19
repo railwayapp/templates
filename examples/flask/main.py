5963022f023aee072424e0e2aa2602ff38962f3b
Jake Runzer <jakerunzer@gmail.com>
2021-02-18 23:35:30 -0800

    update flask response text

--- examples/flask/main.py
+++ examples/flask/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"hello": "world"})
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 if __name__ == '__main__':
