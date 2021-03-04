bf7388bb48fd9fda364e8bd32bfe00e4c92bb59a
amy null <null@amy.gg>
2021-03-03 19:13:39 -0500

    Update prod.exs

--- examples/elixir-phoenix/config/prod.exs
+++ examples/elixir-phoenix/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :railway_phoenix, RailwayPhoenixWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "example.com", port: String.to_integer(System.get_env("PORT") || "80")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
