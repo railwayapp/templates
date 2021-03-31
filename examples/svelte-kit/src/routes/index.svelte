1d5c8ddb97521b258ae25f6d7c41d9e13fae6044
Jake Runzer <jakerunzer@gmail.com>
2021-03-31 00:22:05 -0700

    format svelte kit starter

--- examples/svelte-kit/src/routes/index.svelte
+++ examples/svelte-kit/src/routes/index.svelte
@@ -1,45 +1,48 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  import Counter from "$lib/Counter.svelte";
 </script>
 
 <main>
-	<h1>Hello world!</h1>
+  <h1>Hello world!</h1>
 
-	<Counter />
+  <Counter />
 
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 4rem auto;
+    max-width: 14rem;
+  }
 
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
+  p {
+    max-width: 14rem;
+    margin: 2rem auto;
+    line-height: 1.35;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
 
-		p {
-			max-width: none;
-		}
-	}
+    p {
+      max-width: none;
+    }
+  }
 </style>
