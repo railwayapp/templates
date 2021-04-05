e20197af8f8fa465e4a016c3a7f8761698b9d92f
Faraz Patankar <farazpatankar@gmail.com>
2021-04-06 01:53:55 +0400

    Update expressjs-prisma example (#69)
    * add slack webhook example
    
    * update expressjs-prisma starter

---
+++ examples/slack-webhook/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
+
+app.get("/", async (req, res) => {
+  res.json({ Hello: "World" });
+});
+
+app.post("/webhook", async (req, res) => {
+  /*
+    SLACK_WEBHOOK_URL is an environment variable
+    that should be configured on Railway
+  */
+  if (!process.env.SLACK_WEBHOOK_URL) {
+    res.status(500).json("Please add a Slack webhook URL");
+    return;
+  }
+
+  const body = JSON.stringify({ text: "Hello from the webhook! :rocket:" });
+
+  try {
+    const response = await axios.post(process.env.SLACK_WEBHOOK_URL, body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    res.sendStatus(response.status);
+  } catch (error) {
+    console.error("error", error.response.data);
+    console.error("error", error.response.status);
+    console.error("error", error.response.headers);
+
+    res.sendStatus(error.response.status);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
