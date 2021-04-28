9d556463ed91651f07a0646f0af15db2844d03b0
Jake Runzer <jakerunzer@gmail.com>
2021-04-28 12:21:34 -0700

    Fix Rails Starter (#80)

--- examples/rails-starter/db/schema.rb
+++ examples/rails-starter/db/schema.rb
@@ -14,5 +14,6 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
 end
