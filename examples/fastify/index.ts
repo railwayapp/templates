28083a8e57c6d1c5d18134786c47f4dc1cf08ef3
Faraz Patankar <farazpatankar@gmail.com>
2021-05-31 09:21:43 +0400

    fix fastify starter

--- examples/fastify/index.ts
+++ examples/fastify/index.ts
@@ -6,7 +6,7 @@ server.get("/", async (request, reply) => {
   reply.code(200).send({ message: "Hello world!" });
 });
 
-server.listen(8080, (err, address) => {
+server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
