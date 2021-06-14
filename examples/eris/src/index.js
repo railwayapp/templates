74f60e2dcc0021c4c98987f631076325d9619b5b
Yoshida Tomio <mail@1chi.tk>
2021-06-14 13:30:35 +0800

    add eris example (#131)

---
+++ examples/eris/src/index.js
@@ -0,0 +1,14 @@
+const Eris = require('eris');
+const bot = Eris(`Bot ${process.env.DISCORD_TOKEN}`);
+
+bot.on('ready', () => console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`));
+
+bot.on('messageCreate', (message) => {
+	if (message.content === '!ping') {
+		bot.createMessage(message.channel.id, 'Pong!');
+	} else if (message.content === 'Hello') {
+		bot.createMessage(message.channel.id, 'Choo choo! 🚅');
+	}
+});
+
+bot.connect();
