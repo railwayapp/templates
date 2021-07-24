df75cf57366d0e375c62ad53badb9831158a13be
Evernote <51650634+EvernoteMC@users.noreply.github.com>
2021-07-24 18:01:13 +0530

    Made port an integer

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -17,4 +17,4 @@ def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
     return { "value": rval }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
