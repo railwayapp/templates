fb73c761b0c599395ebf53647ea7e15df6006886
tsutorm <oonishi.wasborn.1981@gmail.com>
2021-11-21 14:43:53 +0900

    bumpup 1.16.2 and std/http 0.114.0

--- examples/deno/src/index.ts
+++ examples/deno/src/index.ts
@@ -1,6 +1,7 @@
-import { listenAndServe } from "https://deno.land/std@0.107.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 
-const port = parseInt(Deno.env.get("PORT") ?? "8000")
-listenAndServe(`:${port}`, () => new Response("Choo Choo! Welcome to your Deno app\n"))
+const port = parseInt(Deno.env.get("PORT") ?? "8000");
+serve(() => new Response("Choo Choo! Welcome to your Deno app\n"),
+      { addr: `:${port}` });
 
 console.log(`http://localhost:${port}/`);
