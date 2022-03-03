87b6d42b9f87bd4146a7d533803c73023c4ccbb5
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2022-03-03 08:45:33 +0530

    Update examples/eris/src/commands/hello.js
    Co-authored-by: Tomio <mail@tomio.codes>

--- examples/eris/src/commands/hello.js
+++ examples/eris/src/commands/hello.js
@@ -2,8 +2,6 @@ module.exports = {
   name: "hello",
   description: "Says hello!",
   async execute(message, args) {
-    message.channel.createMessage(
-      "Hello " + message.author.username + "!" + " Choo choo 🚅",
-    );
+    message.channel.createMessage(`Hello ${message.author.username}!, Choo choo 🚅`);
   },
 };
