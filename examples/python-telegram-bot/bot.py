ecf74b3088a2acbc3fdb4f3680e289663eb5447c
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2022-03-07 03:55:04 +0530

    Update bot.py

--- examples/python-telegram-bot/bot.py
+++ examples/python-telegram-bot/bot.py
@@ -7,13 +7,20 @@ def start(update: Update, _) -> None:
     update.message.reply_text(f"Hello {update.effective_user.first_name}\nI am a sample Telegram bot made with python-telegram-bot powered by railway.app!")
     return
 
-updater = Updater(environ.get("TOKEN"))
+TOKEN = environ.get("TOKEN")
+if not TOKEN:
+    print("No bot token to start the bot!")
+    exit(1)
+updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler("start", start, run_async=True))
 
 print("Bot started successfully!")
 
-updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                      port=int(environ.get("PORT", 84)),
+                      url_path=TOKEN,
+)                     
 updater.idle()
 updater.stop()
 print("Bot stoped successfully!")
