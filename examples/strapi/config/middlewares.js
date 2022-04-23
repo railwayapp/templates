6c86dcfe75eff3c46793be81edb8f561ae28ec5a
wyzlle <101371689+wyzlle@users.noreply.github.com>
2022-04-23 07:50:03 +0000

    Bump strapi to 4.1.8

--- examples/strapi/config/middlewares.js
+++ examples/strapi/config/middlewares.js
@@ -19,6 +19,7 @@ module.exports = [
   'strapi::logger',
   'strapi::query',
   'strapi::body',
+  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
--- examples/strapi/config/plugins.js
+++ examples/strapi/config/plugins.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+      jwtSecret: env('JWT_SECRET'),
+      },
+    },
     upload: {
       config: {
         provider: 'cloudinary',
