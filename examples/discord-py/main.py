e8ef844dd89bc976ad58265d5d8d96af9e376b65
Lonely Guy <phoenex659@gmail.com>
2022-05-03 01:28:04 +0530

    Add message intents to examples
    Added the new requirement of message intents by discord.py

--- examples/discord-py/main.py
+++ examples/discord-py/main.py
@@ -1,7 +1,11 @@
 import os
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
