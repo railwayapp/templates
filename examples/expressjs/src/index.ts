1377357c20e0768526aedbde709505bb18c4be0f
diegofcornejo <diegof.cornejo@gmail.com>
2022-06-17 01:18:38 -0600

    Remove import bodyParser in favor express body-parser middleware

--- examples/expressjs/src/index.ts
+++ examples/expressjs/src/index.ts
@@ -1,12 +1,11 @@
-import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   res.json({ Hello: "World" });
