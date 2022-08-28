e4592f2df7d55d969e14ee0ed3533e3ac5898a87
DeviousLab <deviouslab@gmail.com>
2022-08-28 06:59:56 +0400

    Removed jsonify as a dependency from main.py
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- examples/flask/main.py
+++ examples/flask/main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    return "<p>Welcome to your Flask app! 🚅</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
